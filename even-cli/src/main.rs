043f9a758d104eb9d837e8c3c06decb16d609e62
Timothy Carstens <intoverflow@gmail.com>
2024-01-14 13:45:03 -0800

    Subcommands

--- even-cli/src/main.rs
+++ even-cli/src/main.rs
@@ -3,27 +3,46 @@ use std::time::Duration;
 use alloy_primitives::{FixedBytes, U256};
 use anyhow::{ensure, Context, Result};
 use bonsai_sdk::alpha as bonsai_sdk;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use risc0_zkvm::{serde::to_vec, Receipt};
 
 mod contract;
-mod tx_sender;
 mod seal;
+mod tx_sender;
 
-#[derive(Parser, Debug)]
+#[derive(Clone, Debug, Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    #[clap(short, long, require_equals = true)]
-    chain_id: u64,
+    #[command(subcommand)]
+    command: Command,
+}
 
-    #[clap(short, long, require_equals = true)]
-    rpc_url: String,
+#[derive(Clone, Debug, Subcommand)]
+enum Command {
+    Deploy,
+    TestVector {
+        #[clap(short, long, require_equals = true)]
+        image_id: String,
 
-    #[clap(short, long, require_equals = true)]
-    contract: String,
+        #[clap(short, long, require_equals = true)]
+        number: U256,
+    },
+    SendTx {
+        #[clap(short, long, require_equals = true)]
+        image_id: String,
 
-    #[clap(short, long, require_equals = true)]
-    number: U256,
+        #[clap(short, long, require_equals = true)]
+        chain_id: u64,
+
+        #[clap(short, long, require_equals = true)]
+        rpc_url: String,
+
+        #[clap(short, long, require_equals = true)]
+        contract: String,
+
+        #[clap(short, long, require_equals = true)]
+        number: U256,
+    },
 }
 
 #[tokio::main]
@@ -31,46 +50,70 @@ async fn main() -> Result<()> {
     env_logger::init();
     let args = Args::parse();
 
-    let tx_sender = match std::env::var("ETH_WALLET_PRIVATE_KEY") {
-        Ok(private_key) => Some(tx_sender::TxSender::new(
-            args.chain_id,
-            &args.rpc_url,
-            &private_key,
-            &args.contract,
-        )?),
-        _ => None,
-    };
+    match args.command {
+        Command::Deploy => deploy().await?,
+        Command::TestVector { image_id, number } => {
+            let (seal, post_state_digest) = prove(image_id, number).await?;
 
-    if tx_sender.is_some() {
-        println!("Private key is set; transaction will be sent");
+            println!("number: {}", number);
+            print!("seal: ");
+            for b in &seal {
+                print!("\\x{:02x}", b);
+            }
+            println!("");
+            println!("post_state_digest: {}", post_state_digest);
+        }
+        Command::SendTx {
+            image_id,
+            chain_id,
+            rpc_url,
+            contract,
+            number,
+        } => {
+            let private_key = std::env::var("ETH_WALLET_PRIVATE_KEY")?;
+            let tx_sender = tx_sender::TxSender::new(chain_id, &rpc_url, &private_key, &contract)?;
+
+            let (seal, post_state_digest) = prove(image_id, number).await?;
+
+            tx_sender
+                .send(contract::set(number, seal, post_state_digest))
+                .await?;
+        }
     }
 
-    println!("Number: {}", args.number);
+    Ok(())
+}
 
+fn compute_image_id(elf: &[u8]) -> String {
+    let program = risc0_zkvm::Program::load_elf(elf, risc0_zkvm::GUEST_MAX_MEM as u32)
+        .expect("Could not load ELF");
+    let image = risc0_zkvm::MemoryImage::new(&program, risc0_zkvm::PAGE_SIZE as u32)
+        .expect("Could not create memory image");
+    hex::encode(image.compute_id())
+}
+
+async fn deploy() -> Result<()> {
     let client = bonsai_sdk::Client::from_env(risc0_zkvm::VERSION)?;
 
     // Compute the image_id, then upload the ELF with the image_id as its key.
-    let image_id = {
-        let program = risc0_zkvm::Program::load_elf(
-            even_guests::IS_EVEN_ELF,
-            risc0_zkvm::GUEST_MAX_MEM as u32,
-        )
-        .expect("Could not load ELF");
-        let image = risc0_zkvm::MemoryImage::new(&program, risc0_zkvm::PAGE_SIZE as u32)
-            .expect("Could not create memory image");
-        hex::encode(image.compute_id())
-    };
+    let image_id = compute_image_id(even_guests::IS_EVEN_ELF);
     client.upload_img(&image_id, even_guests::IS_EVEN_ELF.to_vec())?;
-    eprintln!("Image ID: 0x{}", image_id);
+    println!("{}", image_id);
+
+    Ok(())
+}
+
+async fn prove(image_id: String, number: U256) -> Result<(Vec<u8>, FixedBytes<32>)> {
+    let client = bonsai_sdk::Client::from_env(risc0_zkvm::VERSION)?;
 
     // Prepare input data and upload it.
     let input_id = {
-        let input_data = to_vec(&args.number).unwrap();
+        let input_data = to_vec(&number).unwrap();
         let input_data = bytemuck::cast_slice(&input_data).to_vec();
         client.upload_input(input_data)?
     };
 
-    // Start a session running the prover
+    // Fetch the receipt
     let session = client.create_session(image_id, input_id)?;
     eprintln!("Created session: {}", session.uuid);
     let receipt = loop {
@@ -112,9 +155,9 @@ async fn main() -> Result<()> {
 
         let committed_number = U256::from_be_slice(receipt.journal.bytes.as_slice());
         ensure!(
-            args.number == committed_number,
+            number == committed_number,
             "Commitment mismatch: {} != {}",
-            args.number,
+            number,
             committed_number
         );
 
@@ -145,8 +188,6 @@ async fn main() -> Result<()> {
         }
     };
 
-    eprintln!("Snark proof!: {snark_receipt:?}");
-
     // Convert from Bonsai snark format to Eth snark format
     let seal = seal::Seal::abi_encode(snark_receipt.snark).context("Read seal")?;
     let post_state_digest: FixedBytes<32> = snark_receipt
@@ -155,22 +196,5 @@ async fn main() -> Result<()> {
         .try_into()
         .context("Read post_state_digest")?;
 
-    // Print the seal and post_state_digest
-    {
-        print!("seal: ");
-        for b in &seal {
-            print!("\\x{:02x}", b);
-        }
-        println!("");
-        println!("post_state_digest: {}", post_state_digest);
-    }
-
-    // Send the transaction
-    if let Some(tx_sender) = tx_sender {
-        tx_sender
-            .send(contract::set(args.number, seal, post_state_digest))
-            .await?;
-    }
-
-    Ok(())
+    Ok((seal, post_state_digest))
 }
